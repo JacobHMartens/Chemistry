d9a832ffb7b17a54c96e888704627a41b442702b
Agentii <56613955+Agentii@users.noreply.github.com>
2021-08-13 23:54:53 +0200

    Added more methods

--- src/results/ElectronicConfigurations.java
+++ src/results/ElectronicConfigurations.java
@@ -1,28 +1,29 @@
 package results;
 
+import static results.AtomsAndMolecules.getAtomNumberFromElement;
+import static data.DataReader.*;
+
 public class ElectronicConfigurations {
-/*
-
-def getElectronicConfig(element):
-    ion = 0
-    startIdx = element.find("(")
-    if startIdx != -1:
-        endIdx = element.find(")")
-        ion = element[startIdx+1: endIdx]
-        if ion[-1] == "-":
-            ion = int(ion[:-1])
-        else:
-            ion = -int(ion[:-1])
-        element = element[:startIdx]
-
-    atomNumber = getAtomNumberFromElement(element) + ion
-
-    electroConfigShort = Maps.atomicNumberToElectroConfig[atomNumber]
-    electroConfigFull = electroConfigShort
-    while "[" in electroConfigFull:
-        nobleGasAtomicNumber = getAtomNumberFromElement(electroConfigFull[1:3])
-        electroConfigFull = Maps.atomicNumberToElectroConfig[nobleGasAtomicNumber] + electroConfigFull[4:]
-
-    return electroConfigShort, electroConfigFull
- */
+	
+	public static String getElectronicConfig(String element) {
+		int ion = 0;
+		int startIdx = element.indexOf("(") + 1;
+		if (startIdx != -1) {
+			int endIdx = element.indexOf(")") - 1;
+			ion = Integer.valueOf(element.substring(startIdx, endIdx));
+			if (element.charAt(endIdx) == '+')
+				ion *= -1;
+			element = element.substring(0, startIdx-1);	
+		}
+		int atomNumber = getAtomNumberFromElement(element) + ion;
+		
+		String electroConfigShort = atomicNumberToElectroConfig.get(atomNumber);
+		String electroConfigFull = electroConfigShort;
+		while (electroConfigFull.charAt(0) == '[') {
+			int nobleGasAtomicNumber = getAtomNumberFromElement(electroConfigFull.substring(1, 3));
+			electroConfigFull = atomicNumberToElectroConfig.get(nobleGasAtomicNumber) + electroConfigFull.substring(4);
+		}
+	    return electroConfigShort + "   " + electroConfigFull;
+	}
+	// This method is currently incorrect for ions
 }
